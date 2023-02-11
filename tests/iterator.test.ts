1c59561ca83d7a3b74ff710bfa1774e8e75d2c68
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2023-02-11 09:54:57 -0500

    Change tests to use array iterator

--- tests/iterator.test.ts
+++ tests/iterator.test.ts
@@ -1,13 +1,21 @@
 import { describe, expect, test } from 'vitest';
 
-function* testGenerator() {
-  for (let i = 0; i < 3; i++) {
-    yield i;
-  }
-}
-
 describe.each(['next', 'yields'] as const)('.%s', (name) => {
-  test('returns value in chain', () => {
+  test('returns .next().value in chain', () => {
+    const iter = [0, 1, 2].values();
+    expect(iter)[name].toBe(0);
+    expect(iter)[name].toBe(1);
+    expect(iter)[name].toBe(2);
+    expect(iter)[name].toBeUndefined();
+  });
+
+  test('returns values for generator', () => {
+    function* testGenerator() {
+      for (let i = 0; i < 3; i++) {
+        yield i;
+      }
+    }
+
     const iter = testGenerator();
     expect(iter)[name].toBe(0);
     expect(iter)[name].toBe(1);
@@ -24,7 +32,7 @@ describe.each(['next', 'yields'] as const)('.%s', (name) => {
 
 describe('.toBeDone()', () => {
   test('asserts done', () => {
-    const iter = testGenerator();
+    const iter = [0, 1, 2].values();
     expect(iter).next.toBe(0);
     expect(iter).next.toBe(1);
     expect(iter).next.toBe(2);
@@ -32,8 +40,18 @@ describe('.toBeDone()', () => {
     expect(iter).next.toBeDone();
   });
 
-  test('asserts not done', () => {
+  test('asserts done for generator', () => {
+    function* testGenerator() {
+      yield 0;
+    }
+
     const iter = testGenerator();
+    expect(iter).next.not.toBeDone();
+    expect(iter).next.toBeDone();
+  });
+
+  test('asserts not done', () => {
+    const iter = [0, 1, 2].values();
     expect(iter).next.toBe(0);
     expect(iter).next.toBe(1);
 
@@ -42,7 +60,7 @@ describe('.toBeDone()', () => {
   });
 
   test('allows IteratorResult in expect()', () => {
-    const iter = testGenerator();
+    const iter = [0, 1, 2].values();
     const result = iter.next();
     expect(result).toHaveProperty('value', 0);
     expect(result).not.toBeDone();
